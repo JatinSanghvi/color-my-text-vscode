840d96df0ebda2adf02c741ff5e70c7bf0b768b0
Jatin Sanghvi <20547963+JatinSanghvi@users.noreply.github.com>
2022-09-19 22:53:13 +0530

    Apply style as user is typing

--- src/extension.ts
+++ src/extension.ts
@@ -3,28 +3,68 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Extension "color-my-text" is activated.');
 
-	let disposable = vscode.commands.registerCommand('color-my-text.helloWorld', () => {
-
-		const decorationType = vscode.window.createTextEditorDecorationType({
-			color: new vscode.ThemeColor('terminal.ansiRed'),
-		});
-
-		const regExp: RegExp = /Jatin/g;
-
-		vscode.window.visibleTextEditors.forEach(editor => {
-			const text: string = editor.document.getText();
-			const ranges: vscode.Range[] = [];
-
-			let match: RegExpExecArray | null;
-			while (match = regExp.exec(text)) {
-				const start = editor.document.positionAt(match.index);
-				const end = editor.document.positionAt(match.index + match[0].length);
-				ranges.push(new vscode.Range(start, end));
-			}
-
-			editor.setDecorations(decorationType, ranges);
-		});
+	const decorationType = vscode.window.createTextEditorDecorationType({
+		color: new vscode.ThemeColor('terminal.ansiRed'),
 	});
 
-	context.subscriptions.push(disposable);
+	const regExp = /Jatin/g;
+
+	let activeEditor = vscode.window.activeTextEditor;
+
+	function updateDecorations() {
+		if (!activeEditor) {
+			return;
+		}
+
+		const text = activeEditor.document.getText();
+		const ranges: vscode.Range[] = [];
+
+		let match: RegExpExecArray | null;
+		while (match = regExp.exec(text)) {
+			const startPosition = activeEditor.document.positionAt(match.index);
+			const endPosition = activeEditor.document.positionAt(match.index + match[0].length);
+			ranges.push(new vscode.Range(startPosition, endPosition));
+		}
+
+		activeEditor.setDecorations(decorationType, ranges);
+	}
+
+	let timeout: NodeJS.Timer | undefined = undefined;
+
+	function triggerUpdateDecorations(throttle = false) {
+		if (timeout) {
+			clearTimeout(timeout);
+			timeout = undefined;
+		}
+
+		if (throttle) {
+			timeout = setTimeout(updateDecorations, 500);
+		} else {
+			updateDecorations();
+		}
+	}
+
+	if (activeEditor) {
+		triggerUpdateDecorations();
+	}
+
+	// Register event listeners.
+	vscode.window.onDidChangeActiveTextEditor(
+		editor => {
+			activeEditor = editor;
+			if (editor) {
+				triggerUpdateDecorations();
+			}
+		},
+		null,
+		context.subscriptions);
+
+	vscode.workspace.onDidChangeTextDocument(
+		event => {
+			if (activeEditor && event.document === activeEditor.document) {
+				triggerUpdateDecorations(true);
+			}
+		},
+		null,
+		context.subscriptions);
 }

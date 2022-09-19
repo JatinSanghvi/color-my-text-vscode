46e5fd9f3405c620f45ae195f50659c785195a01
Jatin Sanghvi <20547963+JatinSanghvi@users.noreply.github.com>
2022-09-19 14:47:09 +0530

    Apply single style to single text-pattern

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,30 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "color-my-text" is now active!');
+	console.log('Extension "color-my-text" is activated.');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('color-my-text.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Color My Text!');
+
+		const decorationType = vscode.window.createTextEditorDecorationType({
+			color: new vscode.ThemeColor('terminal.ansiRed'),
+		});
+
+		const regExp: RegExp = /Jatin/g;
+
+		vscode.window.visibleTextEditors.forEach(editor => {
+			const text: string = editor.document.getText();
+			const ranges: vscode.Range[] = [];
+
+			let match: RegExpExecArray | null;
+			while (match = regExp.exec(text)) {
+				const start = editor.document.positionAt(match.index);
+				const end = editor.document.positionAt(match.index + match[0].length);
+				ranges.push(new vscode.Range(start, end));
+			}
+
+			editor.setDecorations(decorationType, ranges);
+		});
 	});
 
 	context.subscriptions.push(disposable);
 }
-
-// this method is called when your extension is deactivated
-export function deactivate() {}

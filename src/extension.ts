3c3451b6e85af126889ed9222c0ede5077b060b8
Jatin Sanghvi <20547963+JatinSanghvi@users.noreply.github.com>
2022-09-20 23:27:30 +0530

    Apply style on all text editors

--- src/extension.ts
+++ src/extension.ts
@@ -7,64 +7,51 @@ export function activate(context: vscode.ExtensionContext) {
 		color: new vscode.ThemeColor('terminal.ansiRed'),
 	});
 
+	let doneEditors: vscode.TextEditor[] = [];
+	let todoEditors: vscode.TextEditor[] = vscode.window.visibleTextEditors.slice();
+
 	const regExp = /Jatin/g;
 
-	let activeEditor = vscode.window.activeTextEditor;
-
 	function updateDecorations() {
-		if (!activeEditor) {
-			return;
-		}
+		todoEditors.forEach(todoEditor => {
+			const text = todoEditor.document.getText();
+			let match: RegExpExecArray | null;
+			const ranges: vscode.Range[] = [];
 
-		const text = activeEditor.document.getText();
-		const ranges: vscode.Range[] = [];
-
-		let match: RegExpExecArray | null;
-		while (match = regExp.exec(text)) {
-			const startPosition = activeEditor.document.positionAt(match.index);
-			const endPosition = activeEditor.document.positionAt(match.index + match[0].length);
-			ranges.push(new vscode.Range(startPosition, endPosition));
-		}
-
-		activeEditor.setDecorations(decorationType, ranges);
-	}
-
-	let timeout: NodeJS.Timer | undefined = undefined;
-
-	function triggerUpdateDecorations(throttle = false) {
-		if (timeout) {
-			clearTimeout(timeout);
-			timeout = undefined;
-		}
-
-		if (throttle) {
-			timeout = setTimeout(updateDecorations, 500);
-		} else {
-			updateDecorations();
-		}
-	}
-
-	if (activeEditor) {
-		triggerUpdateDecorations();
-	}
-
-	// Register event listeners.
-	vscode.window.onDidChangeActiveTextEditor(
-		editor => {
-			activeEditor = editor;
-			if (editor) {
-				triggerUpdateDecorations();
+			while (match = regExp.exec(text)) {
+				const startPosition = todoEditor.document.positionAt(match.index);
+				const endPosition = todoEditor.document.positionAt(match.index + match[0].length);
+				ranges.push(new vscode.Range(startPosition, endPosition));
 			}
+
+			console.log("Decorate:", todoEditor.document.fileName);
+			todoEditor.setDecorations(decorationType, ranges);
+			doneEditors.push(todoEditor);
+		});
+
+		todoEditors = [];
+	}
+
+	vscode.window.onDidChangeVisibleTextEditors(
+		visibleEditors => {
+			todoEditors = visibleEditors.filter(visibleEditor => !doneEditors.includes(visibleEditor));
+			doneEditors = doneEditors.filter(doneEditor => visibleEditors.includes(doneEditor));
 		},
 		null,
 		context.subscriptions);
 
 	vscode.workspace.onDidChangeTextDocument(
 		event => {
-			if (activeEditor && event.document === activeEditor.document) {
-				triggerUpdateDecorations(true);
-			}
+			vscode.window.visibleTextEditors.forEach(visibleEditor => {
+				if (visibleEditor.document === event.document && !todoEditors.includes(visibleEditor)) {
+					todoEditors.push(visibleEditor);
+				}
+			});
+
+			doneEditors = doneEditors.filter(doneEditor => !todoEditors.includes(doneEditor));
 		},
 		null,
 		context.subscriptions);
+
+	setInterval(updateDecorations, 500);
 }
